992f29578ef64b0aa98b02b35c783f84a49128c2
sky <hubcarl@126.com>
2020-05-03 16:13:44 +0800

    feat: support @ import file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 'use strict';
+const path = require('path');
+const resolve = filepath => path.resolve(__dirname, filepath);
 module.exports = {
   entry: {
     'admin/login': 'app/web/page/admin/login/login.vue',
     'admin/home': 'app/web/page/admin/home/index.ts'
+  },
+  resolve: {
+    alias:{
+      '@asset': resolve('app/web/asset'),
+      '@framework': resolve('app/web/framework'),
+      '@component': resolve('app/web/component'),
+      '@store': resolve('app/web/page/store'),    
+      '@router': resolve('app/web/page/admin/home/router'),        
+      '@view': resolve('app/web/page/admin/home/view')
+    }
   },
   module:{
     rules:[
